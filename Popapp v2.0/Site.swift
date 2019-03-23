a033de05f9fb736a730ffe895ffee20f7a35b0c2
thinking <thinking@thinkings-Mac-Pro.local>
2019-03-23 12:49:44 -0500

    implentacion de clases site and sitesManager

---
+++ Popapp v2.0/Site.swift
@@ -0,0 +1,40 @@
+import Foundation
+import UIKit
+
+internal struct key {
+    static let title = "title"
+    static let desSite = "desSite"
+    
+}
+
+struct Site{
+    static let defaultImage = UIImage(named: "siteDefault.png")!
+    var title:String
+    var desSite:String
+    private var image:UIImage? = nil
+    var cover:UIImage{
+        get{
+            return image ?? Site.defaultImage
+        }
+        
+        set{
+            image = newValue
+        }
+    }
+    
+    init(title:String, des:String, cover:UIImage? = nil) {
+        self.title = title
+        self.desSite = des
+        self.image = cover
+    }
+    
+    init?(rs:FMResultSet) {
+        guard let title = rs.string(forColumn: key.title),
+            let desSite = rs.string(forColumn: key.desSite)
+            
+            else {return nil }
+        
+        self.init(title:title, des:desSite)
+    }
+    
+}
---
+++ Popapp v2.0/SitesManager.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+//-----------------path for db--------------
+private var appSupportDirectory:URL = {
+    let url = FileManager().urls(for:.applicationSupportDirectory,in: .userDomainMask).first!
+    if !FileManager().fileExists(atPath: url.path) {
+        do {
+            try FileManager().createDirectory(at: url,
+                                              withIntermediateDirectories: false)
+        } catch let error as NSError {
+            print("\(error.localizedDescription)")
+        }
+    }
+    return url
+}()
+
+private var sitesFile:URL = {
+    let filePath = appSupportDirectory.appendingPathComponent("favorites").appendingPathExtension("db")
+    print(filePath)
+    if !FileManager().fileExists(atPath: filePath.path) {
+        if let bundleFilePath = Bundle.main.resourceURL?.appendingPathComponent("favorites").appendingPathExtension("db") {
+            do {
+                try FileManager().copyItem(at: bundleFilePath, to: filePath)
+            } catch let error as NSError {
+                //fingers crossed
+                print("\(error.localizedDescription)")
+            }
+            } }
+        return filePath
+    }()
+
+//class manager --------------------
+class SiteManager{
+    
+}
+
+private func loadBooks()->[Site] {
+    return retrieveSites() ?? []
+}
+
+
+func getOpenDB()->FMDatabase? {
+    let db = FMDatabase(path: sitesFile.path)
+    guard db.open() else {
+        print("Unable to open database")
+        return nil
+    }
+    return db
+    
+}
+
+func retrieveSites() -> [Site]?{
+    guard let db = getOpenDB() else{return nil}
+    var sites:[Site]=[]
+    
+    do{
+        let rs = try db.executeQuery("SELECT * FROM site", values:nil)
+        
+        while rs.next(){
+            if let sitedb = Site(rs: rs){
+                sites.append(sitedb)
+            }
+        }
+    }catch {
+        print("failed: \(error.localizedDescription)")
+    }
+    db.close()
+    return sites
+}
